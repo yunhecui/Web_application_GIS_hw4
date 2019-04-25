e913ee0871881731a674d8afa6c937926fa27594
Yunhe Cui <yc3420@nyu.edu>
2019-04-25 03:50:04 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -80,7 +80,7 @@ map.flyTo({
 document.getElementById('world').addEventListener('click', function () {
 // Fly to a random location by offsetting new york city latlong
 map.flyTo({
-  enter: [150, 35],
+  center: [150, 35],
   zoom: 0.55,
   });
 });
